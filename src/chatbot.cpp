9f59dbeff98b86c19bddd3d7f363dc3eede96cd8
fkballweg <39590424+fkballweg@users.noreply.github.com>
2021-11-07 11:20:05 +0000

    Task 1 and Task 2
    *using unique ptr in chatLogic
    *implemented "Rule of Five" in ChatBot

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -44,6 +44,72 @@ ChatBot::~ChatBot()
 
 //// STUDENT CODE
 ////
+// Copy Constructor
+ChatBot::ChatBot(ChatBot &source)
+{
+    std::cout << "ChatBot Copy Constructor" << std::endl;
+
+    _image = new wxBitmap;
+    *_image = *source._image;
+
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+    _chatLogic->SetChatbotHandle(this)
+}
+
+// Copy Assignment Operator
+ChatBot& ChatBot::operator=(const ChatBot &source)
+{
+    std::cout << "ChatBot Copy Assignment" << std::endl;
+    if (this != &source){
+        delete _image;
+        _image = new wxBitmap;
+        *_image = *source._image;
+
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+        _chatLogic->SetChatbotHandle(this)
+    }
+    return *this;
+}
+
+// Move Constructor
+ChatBot::ChatBot(const ChatBot &&source)  
+{
+    std::cout << "ChatBot Move Constructor" << std::endl;
+
+    _image = source._image;
+    _currentNode = source._currentNode;
+    _rootNode = source._rootNode;
+    _chatLogic = source._chatLogic;
+    _chatLogic->SetChatbotHandle(this)
+    
+    source._image = NULL;
+    source._currentNode = nullptr;
+    source._rootNode = nullptr;
+    source._chatLogic = nullptr;
+}
+
+// Move Assignment Operator
+ChatBot& ChatBot::operator=(ChatBot &&source) 
+{
+    std::cout << "ChatBot Move Assignment" << std::endl;
+    if (this != &source)
+    {
+        if (_image)
+        {
+         delete _image;
+         _image = source._image;
+         _currentNode = source._currentNode;
+         _rootNode = source._rootNode;
+         _chatLogic = source._chatLogic;
+         _chatLogic->SetChatbotHandle(this)
+        }
+    }
+    return *this;
+}
 
 ////
 //// EOF STUDENT CODE
